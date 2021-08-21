7fcd7127db4d57db3ce97501d774c1f3648bb25f
combeysamuel <combeysamuel@gmail.com>
2021-08-21 10:38:05 -0400

    Updated

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 alert(`w and s for left player || up and down for right player`)
 
-const playerSpeed = 15;
+const playerSpeed = 25;
 
-nfp = (urpx) => {
+numberOfPx = (urpx) => {
     return Number(urpx.replace("px", ""))
 }
 
@@ -29,37 +29,37 @@ onkeydown = onkeyup = function(e) {
 keydown = () => {
     //if key was up arrow
     if (map[40]) {
-        if (nfp($rightPlayer.style.top) + playerSpeed > $windowHeight - 200)
+        if (numberOfPx($rightPlayer.style.top) + playerSpeed > $windowHeight - 200)
             $rightPlayer.style.top = $windowHeight - 200 + "px";
         else
-            $rightPlayer.style.top = nfp($rightPlayer.style.top) + playerSpeed + "px";
+            $rightPlayer.style.top = numberOfPx($rightPlayer.style.top) + playerSpeed + "px";
     }
 
 
 
     //if key was down arrow
     else if (map[38]) {
-        if (nfp($rightPlayer.style.top) - playerSpeed < 0)
+        if (numberOfPx($rightPlayer.style.top) - playerSpeed < 0)
             $rightPlayer.style.top = 0 + "px";
         else
-            $rightPlayer.style.top = nfp($rightPlayer.style.top) - playerSpeed + "px";
+            $rightPlayer.style.top = numberOfPx($rightPlayer.style.top) - playerSpeed + "px";
     }
 
 
     //if key was s
     if (map[83]) {
-        if (nfp($leftPlayer.style.top) + playerSpeed > $windowHeight - 200)
+        if (numberOfPx($leftPlayer.style.top) + playerSpeed > $windowHeight - 200)
             $leftPlayer.style.top = $windowHeight - 200 + "px";
         else
-            $leftPlayer.style.top = nfp($leftPlayer.style.top) + playerSpeed + "px";
+            $leftPlayer.style.top = numberOfPx($leftPlayer.style.top) + playerSpeed + "px";
     }
 
     //if key was w
     else if (map[87]) {
-        if (nfp($leftPlayer.style.top) - playerSpeed < 0)
+        if (numberOfPx($leftPlayer.style.top) - playerSpeed < 0)
             $leftPlayer.style.top = 0 + "px";
         else
-            $leftPlayer.style.top = nfp($leftPlayer.style.top) - playerSpeed + "px";
+            $leftPlayer.style.top = numberOfPx($leftPlayer.style.top) - playerSpeed + "px";
     }
 
     //40 down, 38 up
@@ -73,8 +73,8 @@ var balltime = 1;
 $gameBall.style.left = $windowWidth / 2 + "px";
 
 ball = () => {
-    $gameBall.style.left = nfp($gameBall.style.left) + speedx + "px";
-    $gameBall.style.top = nfp($gameBall.style.top) + speedy + "px";
+    $gameBall.style.left = numberOfPx($gameBall.style.left) + speedx + "px";
+    $gameBall.style.top = numberOfPx($gameBall.style.top) + speedy + "px";
 }
 
 
@@ -84,15 +84,15 @@ moveball = () => {
     ball();
 
     //remove overflow y
-    if ($windowHeight < nfp($gameBall.style.top) + 20 || nfp($gameBall.style.top) < 0) {
+    if ($windowHeight < numberOfPx($gameBall.style.top) + 20 || numberOfPx($gameBall.style.top) < 0) {
         speedy *= -1;
     }
 
     //overflow-x right
-    if (nfp($gameBall.style.left) >= $windowWidth - 50) {
-        if (nfp($rightPlayer.style.top) <= nfp($gameBall.style.top) + 20 && nfp($rightPlayer.style.top) + 200 >= nfp($gameBall.style.top)) {
+    if (numberOfPx($gameBall.style.left) >= $windowWidth - 50) {
+        if (numberOfPx($rightPlayer.style.top) <= numberOfPx($gameBall.style.top) + 20 && numberOfPx($rightPlayer.style.top) + 200 >= numberOfPx($gameBall.style.top)) {
             speedx *= -1;
-        } else if (nfp($gameBall.style.left) >= $windowWidth - 20)
+        } else if (numberOfPx($gameBall.style.left) >= $windowWidth - 20)
             goal('left');
     }
 
@@ -100,10 +100,10 @@ moveball = () => {
 
 
     //remove overflow x in left ir get the goal in left
-    if (nfp($gameBall.style.left) <= 30) {
-        if (nfp($leftPlayer.style.top) <= nfp($gameBall.style.top) + 20 && nfp($leftPlayer.style.top) + 200 >= nfp($gameBall.style.top)) {
+    if (numberOfPx($gameBall.style.left) <= 30) {
+        if (numberOfPx($leftPlayer.style.top) <= numberOfPx($gameBall.style.top) + 20 && numberOfPx($leftPlayer.style.top) + 200 >= numberOfPx($gameBall.style.top)) {
             speedx *= -1;
-        } else if (nfp($gameBall.style.left) <= 0)
+        } else if (numberOfPx($gameBall.style.left) <= 0)
             goal('right');
     }
 
