4817d02e726c4d37b2d1c526de7762d8f5084b2a
combeysamuel <combeysamuel@gmail.com>
2021-08-28 00:20:56 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 alert(`w and s for left player || up and down for right player`)
 
-const $playerSpeed = 25
+const playerSpeed = 25
 
 $(() => {
     const $timer = $('.timer')
@@ -15,14 +15,13 @@ $(() => {
         // Display 'counter' wherever you want to display it.
         if (gameTimer <= 0) {
             clearInterval(interval)
-            $timer.hide(300)
             $modal.show()
 
 
-            if (Number($rightScore.innerHTML) > Number($leftScore.innerHTML)) {
+            if (Number(rightScore.innerHTML) > Number(leftScore.innerHTML)) {
                 $won.html(`Right Player Won!!!!!`)
 
-            } else if (Number($rightScore.innerHTML) === Number($leftScore.innerHTML)) {
+            } else if (Number(rightScore.innerHTML) === Number(leftScore.innerHTML)) {
                 $won.html(`NO Player Won!!!!!!!!`)
 
             } else {
@@ -46,18 +45,18 @@ $(() => {
     }
 
 
-    const $rightPlayer = document.getElementById('right')
-    const $leftPlayer = document.getElementById('left')
+    const rightPlayer = document.getElementById('right')
+    const leftPlayer = document.getElementById('left')
 
-    const $gameBall = document.getElementById('ball')
+    const gameBall = document.getElementById('ball')
 
-    const $leftScore = document.getElementById('scoreleft')
-    const $rightScore = document.getElementById('scoreright')
+    const leftScore = document.getElementById('scoreleft')
+    const rightScore = document.getElementById('scoreright')
 
-    const $ogoal = document.getElementById('goal')
+    const ogoal = document.getElementById('goal')
 
-    const $windowWidth = window.innerWidth
-    const $windowHeight = window.innerHeight
+    const windowWidth = window.innerWidth
+    const windowHeight = window.innerHeight
 
     const key = []
 
@@ -74,34 +73,34 @@ $(() => {
 
         //if key pressed was up arrow
         if (key[40]) {
-            if (numberOfPx($rightPlayer.style.top) + $playerSpeed > $windowHeight - 200)
-                $rightPlayer.style.top = $windowHeight - 200 + "px"
+            if (numberOfPx(rightPlayer.style.top) + playerSpeed > windowHeight - 200)
+                rightPlayer.style.top = windowHeight - 200 + "px"
             else
-                $rightPlayer.style.top = numberOfPx($rightPlayer.style.top) + $playerSpeed + "px"
+                rightPlayer.style.top = numberOfPx(rightPlayer.style.top) + playerSpeed + "px"
         }
 
         //if key pressed was down arrow
         else if (key[38]) {
-            if (numberOfPx($rightPlayer.style.top) - $playerSpeed < 0)
-                $rightPlayer.style.top = 0 + "px"
+            if (numberOfPx(rightPlayer.style.top) - playerSpeed < 0)
+                rightPlayer.style.top = 0 + "px"
             else
-                $rightPlayer.style.top = numberOfPx($rightPlayer.style.top) - $playerSpeed + "px"
+                rightPlayer.style.top = numberOfPx(rightPlayer.style.top) - playerSpeed + "px"
         }
 
         //if key pressed was s
         if (key[83]) {
-            if (numberOfPx($leftPlayer.style.top) + $playerSpeed > $windowHeight - 200)
-                $leftPlayer.style.top = $windowHeight - 200 + "px"
+            if (numberOfPx(leftPlayer.style.top) + playerSpeed > windowHeight - 200)
+                leftPlayer.style.top = windowHeight - 200 + "px"
             else
-                $leftPlayer.style.top = numberOfPx($leftPlayer.style.top) + $playerSpeed + "px"
+                leftPlayer.style.top = numberOfPx(leftPlayer.style.top) + playerSpeed + "px"
         }
 
         //if key pressed was w
         else if (key[87]) {
-            if (numberOfPx($leftPlayer.style.top) - $playerSpeed < 0)
-                $leftPlayer.style.top = 0 + "px"
+            if (numberOfPx(leftPlayer.style.top) - playerSpeed < 0)
+                leftPlayer.style.top = 0 + "px"
             else
-                $leftPlayer.style.top = numberOfPx($leftPlayer.style.top) - $playerSpeed + "px"
+                leftPlayer.style.top = numberOfPx(leftPlayer.style.top) - playerSpeed + "px"
         }
 
         //40 down, 38 up
@@ -109,15 +108,14 @@ $(() => {
     }
 
 
-    var speedx = 3,
-        speedy = 1
-    var balltime = 1
+    var ballSpeedxAxis = 3,
+        ballSpeedyAxis = 2
+    var ballTravelTime = 1
 
-    // $gameBall.style.left = $windowWidth / 2 + "px";
 
     ball = () => {
-        $gameBall.style.left = numberOfPx($gameBall.style.left) + speedx + "px"
-        $gameBall.style.top = numberOfPx($gameBall.style.top) + speedy + "px"
+        gameBall.style.left = numberOfPx(gameBall.style.left) + ballSpeedxAxis + "px"
+        gameBall.style.top = numberOfPx(gameBall.style.top) + ballSpeedyAxis + "px"
     }
 
 
@@ -125,30 +123,30 @@ $(() => {
         ball()
 
         //remove overflow y
-        if ($windowHeight < numberOfPx($gameBall.style.top) + 20 || numberOfPx($gameBall.style.top) < 0) {
-            speedy *= -1
+        if (windowHeight < numberOfPx(gameBall.style.top) + 20 || numberOfPx(gameBall.style.top) < 0) {
+            ballSpeedyAxis *= -1
         }
 
         //overflow-x right
-        if (numberOfPx($gameBall.style.left) >= $windowWidth - 50) {
-            if (numberOfPx($rightPlayer.style.top) <= numberOfPx($gameBall.style.top) + 20 && numberOfPx($rightPlayer.style.top) + 200 >= numberOfPx($gameBall.style.top)) {
-                speedx *= -1
-            } else if (numberOfPx($gameBall.style.left) >= $windowWidth - 20)
+        if (numberOfPx(gameBall.style.left) >= windowWidth - 50) {
+            if (numberOfPx(rightPlayer.style.top) <= numberOfPx(gameBall.style.top) + 20 && numberOfPx(rightPlayer.style.top) + 200 >= numberOfPx(gameBall.style.top)) {
+                ballSpeedxAxis *= -1
+            } else if (numberOfPx(gameBall.style.left) >= windowWidth - 20)
                 goal('left')
         }
 
         //remove overflow x in left ir get the goal in left
-        if (numberOfPx($gameBall.style.left) <= 30) {
-            if (numberOfPx($leftPlayer.style.top) <= numberOfPx($gameBall.style.top) + 20 && numberOfPx($leftPlayer.style.top) + 200 >= numberOfPx($gameBall.style.top)) {
-                speedx *= -1
-            } else if (numberOfPx($gameBall.style.left) <= 0)
+        if (numberOfPx(gameBall.style.left) <= 30) {
+            if (numberOfPx(leftPlayer.style.top) <= numberOfPx(gameBall.style.top) + 20 && numberOfPx(leftPlayer.style.top) + 200 >= numberOfPx(gameBall.style.top)) {
+                ballSpeedxAxis *= -1
+            } else if (numberOfPx(gameBall.style.left) <= 0)
                 goal('right')
         }
 
 
         setTimeout(function() {
             moveball()
-        }, balltime)
+        }, ballTravelTime)
     }
 
 
@@ -160,19 +158,19 @@ $(() => {
 
     goal = (positonBallHit) => {
 
-        $ogoal.style.color = "white"
+        ogoal.style.color = "white"
 
         setTimeout(function() {
-            $ogoal.style.color = "black"
+            ogoal.style.color = "black"
         }, 1000)
 
         if (positonBallHit == "left")
-            $leftScore.innerHTML = Number($leftScore.innerHTML) + 1
+            leftScore.innerHTML = Number(leftScore.innerHTML) + 1
         else
-            $rightScore.innerHTML = Number($rightScore.innerHTML) + 1
+            rightScore.innerHTML = Number(rightScore.innerHTML) + 1
 
 
-        speedx *= -1
-        $gameBall.style.left = $windowWidth / 2 + "px"
+        ballSpeedxAxis *= -1
+        gameBall.style.left = windowWidth / 2 + "px"
     }
 })
